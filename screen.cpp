a4c5762b9882d3c3a2763e3016176d0b0609b281
Ivansquark <squark@mail.ru>
2022-11-23 21:08:03 +0300

    test on riscv

--- screen.cpp
+++ screen.cpp
@@ -4,31 +4,100 @@ screen::screen(QWidget* parent) : QDialog(parent) { init(); }
 
 screen::~screen() {
     delete scene;
-    delete butLeft;
-    delete butRight;
-    delete butCenter;
+    delete pixLeft;
+    delete pixRight;
+    delete pixCenter;
+    delete pixCenterStarted;
+    delete pixRightStarted;
+    delete pixLeftStarted;
+    delete scene;
+    delete pixD1; delete pixD2; delete pixD3;
+}
+
+void screen::timeoutFPS()
+{
+    if(counterExit >100) close();
+    if(w > 200) {
+        labScene->setGeometry(w++,0,300,400);
+        labScene->setPixmap(*pixD1);
+    } else {
+        labScene->setGeometry(w++,0,300,400);
+        labScene->setPixmap(*pixD2);
+    }
+
+    switch(currentState) {
+    case States::STOP:
+        currentState = States::STOPPED;
+        break;
+    case States::STOPPED:
+        butLeft->setIcon(*pixLeft);
+        butRight->setIcon(*pixRight);
+        butCenter->setIcon(*pixCenter);
+        currentState = States::START;
+
+
+        break;
+    case States::START:
+        static bool toggle;
+        if(!toggle) {
+            toggle = true;
+            currentState = States::STARTED_LEFT;
+        } else {
+            toggle = false;
+            currentState = States::STARTED_RIGHT;
+        }
+        break;
+    case States::STARTED_LEFT:
+        butLeft->setIcon(*pixLeftStarted);
+        butRight->setIcon(*pixRight);
+        butCenter->setIcon(*pixCenterStarted);
+        currentState = States::STOP;
+        break;
+    case States::STARTED_RIGHT:
+        butLeft->setIcon(*pixLeft);
+        butRight->setIcon(*pixRightStarted);
+        butCenter->setIcon(*pixCenterStarted);
+        currentState = States::STOP;
+        counterExit++;
+        break;
+    }
 }
 
 void screen::init() {
-    setFixedSize(600, 1024);
-    butLeft = new QPixmap(":/pictures/play_left.png");
-    butRight = new QPixmap(":/pictures/play_right.png");
-    butCenter = new QPixmap(":/pictures/stop200.png");
+    setFixedSize(1024, 768);
+    pixLeft = new QPixmap(":/pictures/play_left.png");
+    pixRight = new QPixmap(":/pictures/play_right.png");
+    pixCenter = new QPixmap(":/pictures/stop200.png");
+    pixLeftStarted = new QPixmap(":/pictures/forward_left.png");
+    pixRightStarted = new QPixmap(":/pictures/forward_right.png");
+    pixCenterStarted = new QPixmap(":/pictures/pause.png");
     scene = new QPixmap();
     labScene->setPixmap(*scene);
-    labLeft->setFixedSize(200, 200);
-    labCenter->setFixedSize(200, 200);
-    labRight->setFixedSize(200, 200);
-    labLeft->setIcon(*butLeft);
-    labRight->setIcon(*butRight);
-    labCenter->setIcon(*butCenter);
-    labLeft->setIconSize(QSize(200, 200));
-    labRight->setIconSize(QSize(200, 200));
-    labCenter->setIconSize(QSize(200, 200));
-    layH->addWidget(labLeft);
-    layH->addWidget(labCenter);
-    layH->addWidget(labRight);
+    labScene->setFixedSize(300,400);
+    butLeft->setFixedSize(340, 200);
+    butCenter->setFixedSize(340, 200);
+    butRight->setFixedSize(340, 200);
+    butLeft->setIcon(*pixLeft);
+    butRight->setIcon(*pixRight);
+    butCenter->setIcon(*pixCenter);
+    butLeft->setIconSize(QSize(340, 200));
+    butRight->setIconSize(QSize(340, 200));
+    butCenter->setIconSize(QSize(340, 200));
+    layH->addWidget(butLeft);
+    layH->addWidget(butCenter);
+    layH->addWidget(butRight);
     layV->addWidget(labScene);
     layV->addLayout(layH);
     setLayout(layV);
+
+    //-------------------- timer ----------------------
+    timFPS = new QTimer(this);
+    connect(timFPS, &QTimer::timeout, this, &screen::timeoutFPS);
+    timFPS->start(1); //30fps
+
+    //-------------------- photo ----------------------
+    pixD1 = new QPixmap(":/d1.png");
+    pixD2 = new QPixmap(":/d2.png");
+
+
 }
--- screen.h
+++ screen.h
@@ -1,6 +1,7 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
+#include <qrandom.h>
 #include <QDialog>
 #include <QHBoxLayout>
 #include <QLabel>
@@ -12,22 +13,44 @@
 class screen : public QDialog {
     Q_OBJECT
   public:
+    enum States : uint8_t {
+        STOP,
+        STOPPED,
+        START,
+        STARTED_LEFT,
+        STARTED_RIGHT
+    };
     explicit screen(QWidget* parent = nullptr);
     virtual ~screen();
 
   signals:
+private slots:
+    void timeoutFPS();
   private:
     void init();
     QVBoxLayout* layV = new QVBoxLayout(this);
     QHBoxLayout* layH = new QHBoxLayout(this);
     QPixmap* scene;
-    QPixmap* butLeft;
-    QPixmap* butCenter;
-    QPixmap* butRight;
-    QPushButton* labLeft = new QPushButton(this);
-    QPushButton* labRight = new QPushButton(this);
-    QPushButton* labCenter = new QPushButton(this);
+    QPixmap* pixLeft;
+    QPixmap* pixCenter;
+    QPixmap* pixRight;
+    QPixmap* pixLeftStarted;
+    QPixmap* pixCenterStarted;
+    QPixmap* pixRightStarted;
+    QPixmap* pixD1;
+    QPixmap* pixD2;
+    QPixmap* pixD3;
+    QPushButton* butLeft = new QPushButton(this);
+    QPushButton* butRight = new QPushButton(this);
+    QPushButton* butCenter = new QPushButton(this);
     QLabel* labScene = new QLabel(this);
+    //-------------- timer ---------------
+    QTimer* timFPS;
+    //-------------- states --------------
+    States currentState = States::STOPPED;
+    uint16_t counterExit = 0;
+    //-------------- photo pos -----------
+    uint16_t w = 0;
 };
 
 #endif // SCREEN_H
