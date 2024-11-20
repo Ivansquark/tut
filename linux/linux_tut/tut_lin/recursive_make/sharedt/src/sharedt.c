a426f843e0213188cfb49fb816a2d84ebc34c5ab
ivank <ivank@mrt.ru>
2024-11-20 14:50:38 +0300

    some fixes

--- linux/linux_tut/tut_lin/recursive_make/sharedt/src/sharedt.c
+++ linux/linux_tut/tut_lin/recursive_make/sharedt/src/sharedt.c
@@ -13,7 +13,7 @@ void* pf2(void* arg);
 void print(char* str) {
     pthread_t p1;
     pthread_t p2;
-    pthread_attr_t attr1;
+    // pthread_attr_t attr1;
     pthread_create(&p1, NULL, pf1, str);
     pthread_create(&p2, NULL, pf2, NULL);
     pthread_detach(p2);
@@ -28,18 +28,18 @@ void* pf1(void* arg) {
         printf("%s\r\n", str);
         usleep(1000000);
         count++;
-        if(count >= 5) break;
+        if (count >= 5) break;
     }
     return 0;
 }
 
-void* pf2(void* arg) {
+void* pf2([[maybe_unused]] void* arg) {
     int count = 0;
     while (1) {
         printf("thread 2\r\n");
         usleep(200000);
         count++;
-        //if(count >= 7) break;
+        // if(count >= 7) break;
     }
     return 0;
 }
